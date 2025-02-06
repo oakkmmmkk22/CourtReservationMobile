6896abf1bb2817b371d0cdb290b402aa4ac21f30
Supaiwt <phone2546.pp@gmail.com>
2025-02-06 21:44:27 +0700

    begin a litlle create party

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
-import { Stack } from "expo-router";
+import { Stack} from "expo-router";
+import { View,StyleSheet } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
+
+
   return (
     <Stack initialRouteName="profile"> 
       <Stack.Screen name="index" options={{ title: "Home", headerShown:false }} />
@@ -8,6 +12,47 @@ export default function RootLayout() {
       <Stack.Screen name="profile" options={{ title: "Profile", headerShown:false }} />
       <Stack.Screen name="login" options={{ title: "Login", headerShown:false }} />
       <Stack.Screen name="sign-up" options={{ title: "Sign-Up", headerShown:false }} />
-    </Stack>
+      <Stack.Screen name="createparty" options={{ title: "creaet", headerShown:false }} />
+
+
+        {/* <Stack.Screen
+          name="createparty"
+          options={{
+            title: "Create Party",
+            headerLeft: () => (
+              <View style={myStyleSheet.container}>
+                    <Ionicons
+                      name="chevron-back-outline"
+                      size={30}
+                      color="black"
+                      onPress={() => router.push("/index")} // ฟังก์ชันที่จะเรียกเมื่อกดไอคอน
+                    />
+            </View>
+            ),
+            headerTitleStyle: {
+              fontSize: 30, // ขนาดของข้อความใน header
+              fontWeight: 'bold',
+            },
+            headerStyle: {
+              height: 100, // ขนาดของ header
+              backgroundColor: 'lightgray', // สีพื้นหลังของ header
+            },
+          }}
+        /> */}
+      </Stack>
   );
 }
+// const myStyleSheet = StyleSheet.create({
+//   container: {
+//       flex: 1,
+//       paddingTop: "25%",
+//       paddingLeft:"45%",
+
+//   },
+//   iconTextContainer: {
+//     flexDirection: 'row', 
+//     alignItems: 'center', 
+//     fontWeight:'bold',
+//   },
+
+// })
