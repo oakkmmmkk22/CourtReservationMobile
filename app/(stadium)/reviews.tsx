7cf8ca75c6f49bccaee8dc66ebba1391aac29548
rungwikrai <oakkmmmkk22@gmail.com>
2025-03-06 17:58:17 +0700

    test 5

--- app/(stadium)/reviews.tsx
+++ app/(stadium)/reviews.tsx
@@ -23,7 +23,7 @@ export default function App() {
     const [comment, setComment] = useState("");
     const [reviews, setReviews] = useState<ree[]>([]);
     useEffect(() => {
-        api.get(`/reviews/${idsss.toLocaleString}`)
+        api.get(`/reviews/${idsss.toString()}`)
         // api.get('/reviews/1')
             .then(response => {
                 if (Array.isArray(response.data.data)) {
