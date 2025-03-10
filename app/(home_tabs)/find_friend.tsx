83e7bbef01dfa5a35e40a70eb661c0b1fadf5538
Supaiwt <phone2546.pp@gmail.com>
2025-03-10 22:02:46 +0700

    add config date to now to future

--- app/(home_tabs)/find_friend.tsx
+++ app/(home_tabs)/find_friend.tsx
@@ -78,7 +78,7 @@ const FindFriend = () => {
                                     <View style={styles.modalview1}>
                                         <Calendar style={styles.calendar}
 
-                                            onDayPress={ date => {
+                                            onDayPress={ (date) => {
                                                 //console.log(date);
                                                 let selectedDate = date.dateString;
                                                 // selectedDate.setHours(0, 0, 0, 0);
@@ -86,7 +86,7 @@ const FindFriend = () => {
                                                 setFormattedDate(selectedDate);
                                                 setShowModal(false);
                                             }} 
-                                            minDate={"2025-01-01"}
+                                            minDate={new Date().toISOString().split("T")[0]} // กำหนดให้เลือกได้ตั้งแต่วันนี้
                                             maxDate={"2025-12-31"}
                                         
                                         />
